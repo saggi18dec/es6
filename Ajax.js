64677de4b5a3c9e03652e7c73d7b5776213ce676
Ajinder Singh <ajinder@agilemedialab.in>
2016-02-10 13:43:54 +0530

    class to use xmlHttp in es6

---
+++ Ajax.js
@@ -0,0 +1,33 @@
+class Ajax
+{
+  constructor() {
+    this.requestObject = new XMLHttpRequest();
+    this.method = 'GET';
+    this.async = true;
+    this.url = null;
+  }
+  
+  setMethod(method) {
+    this.method = method;
+    return this;
+  }
+  
+  setUrl(url) {
+    this.url = url;
+    return this;
+  }
+  
+  setAsync(async) {
+    this.async = async;
+    return this;
+  }
+  
+  execute() {
+    this.requestObject.open(this.method, this.url, this.async);
+    this.requestObject.send();
+  }
+}
+
+let myajax = new Ajax();
+myajax.setMethod('GET').setUrl('http://www.abc.com');
+myajax.execute();
